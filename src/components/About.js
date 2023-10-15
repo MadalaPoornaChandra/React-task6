f8a784c68b134f5f692c787f7b7f8d848b75d496
Poorna <poorna@pornhub.gov>
2023-10-15 22:05:38 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+      Hello there. I have a strong enthusiasm for web programming and am presently concentrating on improving my knowledge of Python, the MERN stack, and data structures and algorithms. I'm constantly keen to research and learn about new technologies that could improve my development process.
+      </p>
+
+      <h3>Interests</h3>
+      <p>
+      I have a strong interest in exploring the realm of data structures and algorithms outside of coding. I truly enjoy solving challenging issues and optimising code. In addition, I'm drawn to the MERN stack's potential for creating robust and dynamic web apps.
+      </p>
+
+      <h3>Hobbies</h3>
+      <p>
+      You may frequently find me engrossed in programming tasks or working on individual endeavours in my leisure time. Additionally, I like to read computer-related websites and publications to keep up with the most recent developments in web development. 
+      </p>
+
+      <h3>Studies</h3>
+      <p>
+        I'm currently pursuing my studies in VIT vellore. This academic journey has not only broadened my understanding of various concepts but also provided a strong foundation for my technical endeavors.
+      </p>
+      
+      <h3>Achievements</h3>
+      <ul>
+        <li>achieved a medal in summer cricket tournament in my cricket team and a player from the winner side.</li>
+        <li>awarded as a best student in the year 2019 in 10th class</li>
+      </ul>
+    </section>
+  );
+};
+
+export default About;
